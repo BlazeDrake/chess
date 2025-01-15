819de559ab6efdc14d08a1ec6935a91fa63e8614
BlazeDrake <blaze.drake@outlook.com>
2025-01-15 09:34:23 -0700

    Bishop and queen movement
    Added bishop movement
    This also functionally made queen movement work, as queen just moves with a combination of rooks and bishops

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -110,7 +110,9 @@ public class ChessPiece {
                 return getRookMoves(board,myPosition);
             }
             case QUEEN -> {
-                throw new RuntimeException("Not implemented");
+                var returnSet=getRookMoves(board,myPosition);
+                returnSet.addAll(getBishopMoves(board,myPosition));
+                return returnSet;
             }
             case KNIGHT -> {
                 throw new RuntimeException("Not implemented");
@@ -128,7 +130,75 @@ public class ChessPiece {
     }
 
     private Collection<ChessMove> getBishopMoves(ChessBoard board, ChessPosition myPosition){
-        throw new RuntimeException("Not implemented");
+        var validMoves= new HashSet<ChessMove>();
+        final int col= myPosition.getColumn();
+        final int row= myPosition.getRow();
+        //down and to the left
+        for(int i=1;i<Math.min(row,col);i++){
+            var testPos=new ChessPosition(row-i,col-i);
+
+            var blockingPiece=board.getPiece(testPos);
+
+            if(blockingPiece!=null){
+                if(blockingPiece.getTeamColor()!=getTeamColor()){
+                    validMoves.add(new ChessMove(myPosition,testPos,null));
+                }
+                break;
+            }
+            else{
+                validMoves.add(new ChessMove(myPosition,testPos,null));
+            }
+        }
+        //down and to the right
+        for(int i=1;i<Math.min(row,9-col);i++){
+            var testPos=new ChessPosition(row-i,col+i);
+
+            var blockingPiece=board.getPiece(testPos);
+
+            if(blockingPiece!=null){
+                if(blockingPiece.getTeamColor()!=getTeamColor()){
+                    validMoves.add(new ChessMove(myPosition,testPos,null));
+                }
+                break;
+            }
+            else{
+                validMoves.add(new ChessMove(myPosition,testPos,null));
+            }
+        }
+        //up and to the right
+        for(int i=1;i<Math.min(9-row,9-col);i++){
+            var testPos=new ChessPosition(row+i,col+i);
+
+            var blockingPiece=board.getPiece(testPos);
+
+            if(blockingPiece!=null){
+                if(blockingPiece.getTeamColor()!=getTeamColor()){
+                    validMoves.add(new ChessMove(myPosition,testPos,null));
+                }
+                break;
+            }
+            else{
+                validMoves.add(new ChessMove(myPosition,testPos,null));
+            }
+        }
+        //up and to the left
+        for(int i=1;i<Math.min(9-row,col);i++){
+            var testPos=new ChessPosition(row+i,col-i);
+
+            var blockingPiece=board.getPiece(testPos);
+
+            if(blockingPiece!=null){
+                if(blockingPiece.getTeamColor()!=getTeamColor()){
+                    validMoves.add(new ChessMove(myPosition,testPos,null));
+                }
+                break;
+            }
+            else{
+                validMoves.add(new ChessMove(myPosition,testPos,null));
+            }
+        }
+
+        return validMoves;
     }
     private Collection<ChessMove> getRookMoves(ChessBoard board, ChessPosition myPosition){
         var validMoves= new HashSet<ChessMove>();
