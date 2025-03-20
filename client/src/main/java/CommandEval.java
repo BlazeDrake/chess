1cd4888920bbb25f30fa385806a9c97b7894dd6d
BlazeDrake <blaze.drake@outlook.com>
2025-03-20 09:00:36 -0600

    Login, logout, register
    Added ability to login and logout, as wella s register

--- client/src/main/java/CommandEval.java
+++ client/src/main/java/CommandEval.java
@@ -1,3 +1,4 @@
+import network.datamodels.UserData;
 import server.ResponseException;
 import server.ServerFacade;
 import ui.EscapeSequences;
@@ -9,6 +10,7 @@ public class CommandEval {
     private ServerFacade facade;
     private boolean loggedIn;
     private String username;
+    private String authToken;
 
     public CommandEval(ServerFacade facade) {
         this.facade = facade;
@@ -39,7 +41,8 @@ public class CommandEval {
     }
 
     private String loggedOutCommand(String input) throws ResponseException {
-        return switch (input) {
+        String[] args = input.split(" ");
+        return switch (args[0]) {
             case "help" -> "Logged out commands: \n" +
                     commandInfo("help",
                             "Displays this dialog",
@@ -54,15 +57,60 @@ public class CommandEval {
                     commandInfo("quit",
                             "Exits the client",
                             "quit");
+            case "register" -> {
+                checkCount(args.length, 4);
+                facade.register(new UserData(args[1], args[2], args[3]));
+                yield "User " + args[1] + " successfully registered";
+            }
+            case "login" -> {
+                checkCount(args.length, 3);
+                var result = facade.login(new UserData(args[1], args[2], null));
+                loggedIn = true;
+                username = result.username();
+                authToken = result.authToken();
+                yield "Logged in successfully. Welcome " + result.username();
+            }
+
             case "quit" -> "Quitting client. Goodbye";
             default -> throw new ResponseException(400, "Unknown Command " + input);
         };
     }
 
+    private static void checkCount(int length, int expected) throws ResponseException {
+        if (length != expected) {
+            throw new ResponseException(400, "Wrong number of args");
+        }
+    }
+
     private String loggedInCommand(String input) throws ResponseException {
-        return switch (input) {
-            case "help" -> EscapeSequences.SET_TEXT_BOLD_AND_BLUE +
-                    "Logged in commands: \n";
+        String[] args = input.split(" ");
+        return switch (args[0]) {
+            case "help" -> "Logged in commands: \n" +
+                    commandInfo("help",
+                            "Displays this dialog",
+                            "help") +
+                    commandInfo("list",
+                            "Lists info on all current games",
+                            "list") +
+                    commandInfo("create",
+                            "Create a new game",
+                            "create <gameName>") +
+                    commandInfo("join",
+                            "Join an existing game as a player",
+                            "join <gameID> <team (WHITE | BLACK)>") +
+                    commandInfo("observe",
+                            "Observe an existing game",
+                            "observe ,gameID>") +
+                    commandInfo("logout",
+                            "Log out.",
+                            "logout");
+            case "logout" -> {
+                facade.logout(authToken);
+                username = loggedOutString;
+                authToken = "";
+                loggedIn = false;
+                yield "Logged out successfully. Goodbye";
+            }
             case "quit" -> throw new ResponseException(400, "Must log out before quitting");
             default -> throw new ResponseException(400, "Unknown Command " + input);
         };
@@ -76,7 +124,7 @@ public class CommandEval {
 
     private void handleError(ResponseException ex) {
         String msg = switch (ex.StatusCode()) {
-            case 400 -> "Error: unknown command. Type help to see a list of commands";
+            case 400 -> "Error: unknown or malformatted command. Type help to see a list of commands";
             case 401 -> "Error: unauthorized";
             case 403 -> "Error: already taken";
             default -> "Internal error";
